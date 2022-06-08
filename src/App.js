f33821bd5cf630d56265329a9a664f203646801d
RatonRoy <royraton18@gmail.com>
2022-06-09 04:20:27 +0600

    add the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Pages/Home/Home';
+import Navbar from './components/Pages/Shared/Navbar/Navbar';
 
 
 function App() {
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element = {<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<h1 className=''> This is a new home pages </h1>
+		</div>
+	);
+};
+
+export default Home;
